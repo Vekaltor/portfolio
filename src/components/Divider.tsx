7d5a743648b4e402167c2294883a5d8dd3706f2c
Vekaltor <56607344+Vekaltor@users.noreply.github.com>
2026-03-19 20:35:37 +0100

    IMPLEMENTED content app

---
+++ src/components/Divider.tsx
@@ -0,0 +1,10 @@
+function Divider() {
+    return (
+        <div
+            className="relative z-[1]"
+            style={{ height: '1px', background: 'var(--border)' }}
+        />
+    )
+}
+
+export default Divider
