4039f86a558cbd2285e920ff1b5259b56c18e829
Vekaltor <56607344+Vekaltor@users.noreply.github.com>
2026-03-20 13:01:51 +0100

    IMPLEMENTED animated scrollbar

--- src/AppInitUiPlugins.tsx
+++ src/AppInitUiPlugins.tsx
@@ -1,6 +1,7 @@
 import Sparkles from "./components/effects/Sparkles.tsx";
 import Cursor from "./components/effects/Cursor.tsx";
 import type {ReactNode} from "react";
+import useActiveScroll from "./hooks/useActiveScroll.hook.ts";
 
 interface AppInitUiPluginsProps {
     children: ReactNode
@@ -8,6 +9,8 @@ interface AppInitUiPluginsProps {
 
 function AppInitUiPlugins(props: AppInitUiPluginsProps) {
     const {children} = props
+    useActiveScroll();
+
     return (
         <>
             <Sparkles/>
